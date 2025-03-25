4c55373dc00f65d282c3442bf0ecc4034d34ba71
1DAM-11 <1DAM-11@1ASIR-11.mshome.net>
2025-03-25 20:59:54 +0100

    Casi Terminado

--- Rana/src/rana/JuegoRana.java
+++ Rana/src/rana/JuegoRana.java
@@ -3,28 +3,27 @@ package rana;
 import java.util.Scanner;
 
 public class JuegoRana {
-	
-	
-	public static double iniciar() {
-		Scanner sc = new Scanner(System.in);
-		
-		boolean seguir=true;
-		while(seguir) {
-			System.out.println("Introduce las cordenadas donde quieres disparar");
-			String cordenadas = sc.next();
-			char cordenadaX = cordenadas.charAt(0);
-			char cordenadaY = cordenadas.charAt(1);
-			seguir=Tablero.caza(cordenadaX,cordenadaY);
-			
-		}
-		return 0;
-	}
 
 	public static void main(String[] args) {
 
-		iniciar();
-		
-		
+		Scanner sc = new Scanner(System.in);
+		Tablero estanque = new Tablero();
+
+		boolean seguir = true;
+		while (seguir) {
+			System.out.println("Introduce las cordenadas donde quieres disparar");
+			System.out.println(estanque.toString());
+			String cordenadas = sc.next();
+			char cordenadaX = cordenadas.charAt(0);
+			char cordenadaY = cordenadas.charAt(1);
+			int x = Character.getNumericValue(cordenadaX);
+			int y = Character.getNumericValue(cordenadaY);
+			seguir = estanque.caza(x, y);
+			if(seguir=false) {
+				System.out.println("Rana cazada");
+			}
+				
+		}
 
 	}
 
--- Rana/src/rana/Tablero.java
+++ Rana/src/rana/Tablero.java
@@ -1,6 +1,5 @@
 package rana;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Tablero {
@@ -10,10 +9,17 @@ public class Tablero {
 	public Tablero() {
 
 		this.estanque = new boolean[5][5];
+		posicionarRana();
 
 	}
 
-	public boolean[][] rana() {
+	private boolean[][] posicionarRana() {
+		r = new Random();
+		for (int i = 0; i < estanque.length; i++) {
+			for (int j = 0; j < estanque.length; j++) {
+				estanque[i][j] = false;
+			}
+		}
 
 		estanque[r.nextInt(5)][r.nextInt(5)] = true;
 
@@ -21,20 +27,62 @@ public class Tablero {
 
 	}
 
-	public boolean caza(char cordenadaX, char cordenadaY) {
+	public boolean caza(int x, int y) {
 
-		if (estanque[cordenadaX][cordenadaY] == true) {
+		if (estanque[x][y] == true) {
 			return true;
 
 		} else {
+			if (comprobarSiSalta(x, y)) {
+
+			}
 			return false;
 
 		}
 	}
 
+	private boolean comprobarSiSalta(int x, int y) {
+
+		int rana[] = new int[2];
+		for (int i = 0; i < estanque.length; i++) {
+			for (int j = 0; j < estanque.length; j++) {
+				if (estanque[i][j] == true)
+					rana[0] = i;
+				rana[1] = j;
+			}
+		}
+
+		if ((x == rana[0] && (y + 1 == rana[1] || y - 1 == rana[1])))
+			posicionarRana();
+		else if ((y == rana[1] && (x + 1 == rana[0] || x - 1 == rana[0])))
+			posicionarRana();
+		else if ((y + 1 == rana[1] || y - 1 == rana[1]) && (x + 1 == rana[0] || x - 1 == rana[0]))
+			posicionarRana();
+		else if (x == rana[0] && y == rana[1])
+			return false;
+
+		return true;
+	}
+
 	@Override
 	public String toString() {
-		return "Tablero [estanque=" + Arrays.toString(estanque) + "]";
+		String representar = "";
+		representar += "   0 1 2 3 4\n";
+		for (int i = 0; i < estanque.length; i++) {
+			for (int j = 0; j < estanque.length; j++) {
+				if (estanque[i][j] == true) {
+					representar += "[r]";
+				}
+
+				else if (j == 0) {
+					representar += i + " ";
+				} else
+					representar += "[]";
+			}
+			representar += "[]\n";
+		}
+		return representar;
+
 	}
 
 }
